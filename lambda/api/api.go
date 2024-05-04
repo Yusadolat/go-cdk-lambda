d4913a6a6a7a0e826ead5f2248d049a1582c0e5c
Yusadolat <yusadolat@gmail.com>
2024-05-04 21:06:45 +0100

    add db session with dynamo

--- lambda/api/api.go
+++ lambda/api/api.go
@@ -1,1 +1,13 @@
 package api
+
+import (
+	"lambda-func/database"
+)
+
+type ApiHandler struct {
+dbStore database.DynamoDBClient
+}
+
+funct NewApiHandler (dbStore string) ApiHandler(
+
+)
--- lambda/database/database.go
+++ lambda/database/database.go
@@ -1,1 +1,19 @@
 package database
+
+import (
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+type DynamoDBClient struct {
+	databaseStore *dynamodb.DynamoDB
+
+}
+
+func newDynamoDbClient() DynamoDBClient {
+	dbDession := session.Must(session.NewSession())
+	db : dynamodb.New(dbSession)
+	return DynamoDBClient{
+		databaseStore: "DynamoDB",
+	}
+}
