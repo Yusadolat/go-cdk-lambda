9115ef63638594df0d03fed3aec8f9d69eb371bd
Yusadolat <yusadolat@gmail.com>
2024-05-04 21:26:12 +0100

    chore: Refactor database.go to use dependency injection for DynamoDBClient creation

--- lambda/database/database.go
+++ lambda/database/database.go
@@ -1,19 +1,19 @@
 package database
 
 import (
-	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 type DynamoDBClient struct {
-	databaseStore *dynamodb.DynamoDB
+	databaseStore dynamodb.DynamoDB
 
 }
 
 func newDynamoDbClient() DynamoDBClient {
-	dbDession := session.Must(session.NewSession())
-	db : dynamodb.New(dbSession)
+	dbSession := session.Must(session.NewSession())
+	db := dynamodb.New(dbSession)
 	return DynamoDBClient{
-		databaseStore: "DynamoDB",
+		databaseStore: db,
 	}
 }
